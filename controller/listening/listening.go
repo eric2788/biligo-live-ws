6bf2e7a75d0acb5cf1f9fdba99317cf8691642de
eric2788 <tsukiko852@gmail.com>
2022-04-10 18:17:22 +0800

    fix /listening/:room_id show empty content

--- controller/listening/listening.go
+++ controller/listening/listening.go
@@ -11,16 +11,16 @@ var log = logrus.WithField("controller", "listening")
 
 func Register(gp *gin.RouterGroup) {
 	gp.GET("", GetListening)
-	gp.GET("/:room_id")
+	gp.GET("/:room_id", GetListenRoom)
 }
 
 func GetListenRoom(c *gin.Context) {
 
-	id, err := strconv.ParseInt(c.Param(":room_id"), 10, 64)
+	id, err := strconv.ParseInt(c.Param("room_id"), 10, 64)
 
 	if err != nil {
 		c.IndentedJSON(400, gin.H{
-			"error": "room_id 必須為數字",
+			"error": err.Error(),
 		})
 		return
 	}
@@ -29,7 +29,7 @@ func GetListenRoom(c *gin.Context) {
 
 	if err != nil {
 		log.Warnf("嘗試獲取房間 %v 的直播資訊時出現錯誤: %v", id, err)
-		c.IndentedJSON(500, gin.H{
+		c.IndentedJSON(400, gin.H{
 			"error": err.Error(),
 		})
 		return
